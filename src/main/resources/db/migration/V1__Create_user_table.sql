f6c7b6e4412caac282ccbb08e5ef64ded3eb6f1c
codedrinker <695591519@qq.com>
2019-08-06 10:29:11 +0800

    add flyway migration support

---
+++ src/main/resources/db/migration/V1__Create_user_table.sql
@@ -0,0 +1,21 @@
+create table USER
+(
+  ID           INTEGER  auto_increment,
+  ACCOUND_ID   VARCHAR(100),
+  NAME         VARCHAR(50),
+  TOKEN        CHAR(36),
+  GMT_CREATE   BIGINT,
+  GMT_MODIFIED BIGINT,
+  constraint USER_PK
+    primary key (ID)
+);
+--
+-- comment on column USER.ID is '主键';
+--
+-- comment on column USER.ACCOUND_ID is '账户ID';
+--
+-- comment on column USER.NAME is '用户名';
+--
+-- comment on column USER.GMT_CREATE is '创建时间';
+--
+-- comment on column USER.GMT_MODIFIED is '更改时间';
